a82ab2e3ecd63a5f5f21e2ba011031bac1a1199d
samsel <samsel>
2009-12-17 10:50:24 +0000

    linux um-enhanced-netem merged with .1

--- include/linux/pkt_sched.h
+++ include/linux/pkt_sched.h
@@ -473,22 +473,22 @@ enum
 
 struct tc_netem_qopt
 {
-	__u32	latency;	/* added delay (us) */
-	__u32   limit;		/* fifo limit (packets) */
-	__u32	loss;		/* random packet loss (0=none ~0=100%) */
-	__u32	gap;		/* re-ordering gap (0 for none) */
-	__u32   duplicate;	/* random packet dup  (0=none ~0=100%) */
-	__u32	jitter;		/* random jitter in latency (us) */
-        __u32   reorderdelay;   /* delay for reordered packets (us) */
+	__u32	latency;		/* added delay (us) */
+	__u32   limit;			/* fifo limit (packets) */
+	__u32	loss;			/* random packet loss (0=none ~0=100%) */
+	__u32	gap;			/* minimum re-ordering gap (0 for none) */
+	__u32   duplicate;		/* random packet dup  (0=none ~0=100%) */
+	__u32	jitter;			/* random jitter in latency (us) */
+        __u32   reorderdelay;   	/* delay for reordered packets (us) */
 	__u32	reorderdelayjitter;	/* random jitter for reordered packets (us) */
 };
 
 struct tc_netem_corr
 {
-	__u32	delay_corr;	/* delay correlation */
+	__u32	delay_corr;		/* delay correlation */
 	__u32	reorderdelay_corr;	/* reorderdelay correlation */
-	__u32	loss_corr;	/* packet loss correlation */
-	__u32	dup_corr;	/* duplicate correlation  */
+	__u32	loss_corr;		/* packet loss correlation */
+	__u32	dup_corr;		/* duplicate correlation  */
 };
 
 struct tc_netem_reorder
